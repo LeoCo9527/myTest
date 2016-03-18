27e0257026b51c3716cda415a402a1a7c8792fe9
fmtech <fmtech@fmtechdemac-mini.local>
2016-03-18 15:37:04 +0800

    Initial Commit

---
+++ testGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testGit
+//
+//  Created by fmtech on 16/3/18.
+//  Copyright © 2016年 longjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testGit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testGit
+//
+//  Created by fmtech on 16/3/18.
+//  Copyright © 2016年 longjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
